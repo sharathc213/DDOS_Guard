5d21292a5863d7f23c4b68b7306ec5078915a2d4
sharathc213 <63504490+sharathc213@users.noreply.github.com>
2024-04-08 14:44:45 +0530

    Update config.php

--- DDOS Guard/config.php
+++ DDOS Guard/config.php
@@ -28,7 +28,7 @@ $config = array(
         'facebot',
         'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)',
         'ia_archiver',
-        'firefox'
+      //  'firefox'
     ),
 
     //Template
